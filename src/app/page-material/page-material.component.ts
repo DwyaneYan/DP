7935642cabc06e6e77dcd456ae8f88cea06768a7
wangyao123261 <2597589791@qq.com>
2019-12-17 15:17:58 +0800

    master

--- src/app/page-material/page-material.component.ts
+++ src/app/page-material/page-material.component.ts
@@ -11,33 +11,46 @@ export class PageMaterialComponent implements OnInit {
 
   //冷轧
   public lengzha = [
-    {value: 'IF钢'},
-    {value: '双相钢'},
-    {value: '马氏体钢'},
-    {value: '铸铁'},
+    {value: '烘烤硬化钢'},
+    {value: '高强IF钢'},
     {value: '低碳铝镇静钢'},
-    {value: '其他'},
+    {value: '低合金高强度钢'},
+    {value: '双相钢'},
+    {value: '淬火延性钢'},
+    {value:'马氏体'},
+    {value:'增强成形性双相钢'},
+    {value:'IF钢'}
   ]
 
   //镀锌
   public duxin = [
-    {value: '镀锌材料1'},
-    {value: '镀锌材料2'},
-    {value: '其他'},
+    {value: '烘烤硬化钢'},
+    {value: '高强IF钢'},
+    {value: '低碳铝镇静钢'},
+    {value:'低合金高强度钢'},
+    {value:'双相钢'},
+    {value:'IF钢'},
+    {value:'增强成形性双相钢'}
   ]
 
   //热轧
   public rezha = [
-    {value: '热轧材料1'},
-    {value: '热轧材料2'},
-    {value: '其他'},
+    {value: '低碳钢'},
+    {value: 'SAPH系列'},
+    {value: 'QStE结构钢系列'},
+    {value:'大梁钢'},
+    {value:'车轮钢'},
+    {value:'热轧双相钢'},
+    {value:'箱体钢'},
+    {value:'制动鼓用钢'}
   ]
 
-  //中板
+  //中厚板
   public zhongban = [
-    {value: '中板材料1'},
-    {value: '中板材料2'},
-    {value: '其他'},
+    {value: '大梁钢'},
+    {value: '车轮钢'},
+    {value: '车桥钢'},
+    {value:'自卸车厢体用耐磨钢'}
   ]
   
   
@@ -48,40 +61,41 @@ export class PageMaterialComponent implements OnInit {
       value: '邯钢',
     },
     {
-      value: '宝钢',
+      value: 'BG',
     },
     {
-      value: '马钢',
-    }
+      value: 'SG',
+    },
+    {value:'其他'}
   ]
     //型号规格
   public listType = [    
     {
-      value: '0.6mm',
+      value: '0.7mm',
     },
     {
       value: '1.2mm',
     },
     {
-      value: '2.0mm',
+      value: '1.5mm',
     },
     {
-      value: '2.5~5.4mm',
+      value: '6.0mm',
     },
   ]
-    //强度
+    //屈服强度
   public listStrength = [    
     {
-      value: '200~500MPa',
+      value: '120~180MPa',
     },
     {
-      value: '500~800MPa',
+      value: '180~340MPa',
     },
     {
-      value: '800~1200MPa',
+      value: '340~500MPa',
     },
     {
-      value: '>1200MPa',
+      value: '500~1200MPa',
     },
   ]
 
