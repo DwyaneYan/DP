ea9bb6f3bbfe978b12152c2e6c55423965a368f1
DwyaneYan <996880320@qq.com>
2019-12-31 17:44:42 +0800

    实现静态拉伸数据查询

--- src/app/form-experimental-item/experimental-item.service.ts
+++ src/app/form-experimental-item/experimental-item.service.ts
@@ -11,13 +11,15 @@ export class ExperimentalItemService {
   ) { }
 
   async GetTrialItemByMaterialId(params){
-    console.log(params)
-    let api = "http://localhost:60001/api/hangang/materialTrial/trialItemByMaterialId"
-    let res = await this.http.get(api, {params})
+    // console.log(params)
+    let materialId = params.Materiald
+    let api = `http://localhost:60001/api/hangang/materialTrial/trialItemByMaterialId/${materialId}`
+    let res = await this.http.get(api)
     .toPromise()
     .catch(err=>{
       console.log(err)
     });
+    // console.log(res)
     return res;
   }
 }
--- src/app/form-trial-details/details-data/details-data.service.ts
+++ src/app/form-trial-details/details-data/details-data.service.ts
@@ -14,17 +14,17 @@ export class DetailsDataService {
   ) { }
 
   // 查询实验数据明细
-  public async GetTrialDataDetails(params,materialTrial)
+  public async GetTrialDataDetails(params)
   {
-    let materialId = materialTrial
-    let api =`http://localhost:60001/api/hangang/materialTrial/trialDataDetails/${materialId}`;
-    
-    let res= await this.http.get(api, {params})
+    let materialId = params
+    let api =`http://localhost:60001/api/hangang/materialTrial/staticTensionDataDetails/${materialId}`;
+    console.log(params)
+    let res= await this.http.get(api)
     .toPromise()
     .catch(err =>{
       console.log(err);
     });
-    console.log(res);
+    // console.log(res);
     return res;  //返回请求到的数据(Promise对象)
   }
 
