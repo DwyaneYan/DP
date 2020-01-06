95f8c5957b9d8029a2b98a7826056f32f80df52c
YanWen <996880320@qq.com>
2020-01-06 19:53:03 +0800

    按材料名称搜索, 请求到数据

--- src/app/form-material-db-background/form-material-db-background.component.ts
+++ src/app/form-material-db-background/form-material-db-background.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-form-material-db-background',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormMaterialDbBackgroundComponent implements OnInit {
 
-  constructor() { }
+  //存放查询到的材料
+  materials
+
+  public params = {
+    Name: '', 
+  }
+
+
+  constructor(
+    public http: HttpClient,
+
+  ) { }
 
   ngOnInit() {
+  }
+
+  public async SearchMaterialByName(){
+    let params = this.params
+    let api = "http://localhost:60001/api/hangang/material/materials";
+    await this.http.get(api, {params})
+    .toPromise()
+    .then((res:any) =>{
+      this.materials = res.items
+    })
+    console.log(this.materials)
   }
 
 }
