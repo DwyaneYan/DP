10fcd0a973791b4e69fd85b2aecb9b7c692d7cd4
wangyao123261 <2597589791@qq.com>
2020-01-17 09:36:58 +0800

    17

--- src/app/form-trial-details/static-tension-home/static-tension-picture/static-tension-picture.component.ts
+++ src/app/form-trial-details/static-tension-home/static-tension-picture/static-tension-picture.component.ts
@@ -1,28 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-
-
 @Component({
   selector: 'app-static-tension-picture',
   templateUrl: './static-tension-picture.component.html',
   styleUrls: ['./static-tension-picture.component.css']
 })
 export class StaticTensionPictureComponent implements OnInit {
-
   materialId 
   trialDataDetail=[]
-
   trialDataDetails=[]
   public listOfId = ["16c75e5c-75fd-4a0c-8a3c-ff5957303608","338679ba-3049-42ba-b7e8-c13cec76cdd2","dae51c76-d308-482f-a1f8-178a2c7f1a5f"]  //存放不同序号的id
   public dataList1=[]
   public dataList2=[]
   public dataList3=[]
-
   //echarts绘图
   option:any;
-
-
   constructor(
     public http: HttpClient,
     private router: Router,
