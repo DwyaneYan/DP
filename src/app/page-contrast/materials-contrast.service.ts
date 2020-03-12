998e702e554a582f78a2fccde8e3bbce961be6a0
wangyao123261 <2597589791@qq.com>
2020-03-12 20:33:04 +0800

    contrast

---
+++ src/app/page-contrast/materials-contrast.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class MaterialsContrastService {
+
+  constructor(public http: HttpClient) { }
+  async GetMaterials(array)
+  {
+    if(array.length>1){
+      var api
+      api = "http://localhost:60001/api/hangang/GetStaticTensionDataDetailsNum?ids=";
+    api +=array[0];
+    for(var j=1;j<array.length;j++){   
+    api+="&ids="+array[j]
+    }
+    let res= await this.http.get(api).toPromise().catch(err=>{
+      console.log(err);        
+    });
+    console.log(api) ;
+    console.log(res) 
+    return res;    
+  }
+
+else{
+  let api = "http://localhost:60001/api/hangang/GetStaticTensionDataDetailsNum?ids=";
+  let res= await this.http.get(api+array[0]).toPromise().catch(err=>{
+    console.log(err);
+  });
+  return res;
+}
+  }
+  async GetMaterialss(params)
+  {
+    let api ="http://localhost:60001/api/hangang/material/materials?Id=";
+    let res= await this.http.get(api+params)
+    .toPromise()
+    .catch(err =>{
+      console.log(err);
+    });
+    return res;  //返回请求到的数据(Promise对象)
+  }
+}
--- src/app/page-contrast/page-contrast.component.ts
+++ src/app/page-contrast/page-contrast.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 import {Router ,ActivatedRoute} from '@angular/router';
+import {MaterialsContrastService} from './materials-contrast.service';
+
 @Component({
   selector: 'app-page-contrast',
   templateUrl: './page-contrast.component.html',
@@ -8,9 +10,14 @@ import {Router ,ActivatedRoute} from '@angular/router';
 })
 
 export class PageContrastComponent implements OnInit {
-
+  listMaterials=[]
+  name= [];
+  model=[];
+  manu=[]
   constructor( private router: Router,
-    private routerinfo:ActivatedRoute) { }
+    private routerinfo:ActivatedRoute,
+    private MaterialsContrastService:MaterialsContrastService,
+    ) { }
     contrastID
     array=[]
   ngOnInit() {  
@@ -19,7 +26,30 @@ export class PageContrastComponent implements OnInit {
       this.contrastID = this.routerinfo.snapshot.params['contrastID']
       console.log(this.contrastID)
       this.array=this.contrastID.split(",");
-      console.log(this.array)
+      console.log(this.array);
+      this.getGetMaterialss()
+      this.getGetMaterials();
+      
   }
+
+  public async getGetMaterials() {     
+    for(var i=0;i<this.array.length;i++){
+    await this.MaterialsContrastService.GetMaterials(this.array).then((res: any) => {
+      this.listMaterials[i] = res[i]; 
+      console.log(this.listMaterials)
+    });
+}
+  }
+  public async getGetMaterialss() {     
+    for(var i=0;i<this.array.length;i++){
+    await this.MaterialsContrastService.GetMaterialss(this.array[i]).then((res: any) => {
+      this.name[i] = res.items[0].name; 
+      this.model[i] = res.items[0].model;
+      this.manu[i] = res.items[0].manufactoryName;
+      
+    });
+}
+  }
+
 
 }
