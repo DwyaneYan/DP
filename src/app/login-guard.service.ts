dea1211eb94016b3634637f443a6d17c70b17dcb
wangyao123261 <2597589791@qq.com>
2020-11-09 10:54:18 +0800

    。。。

--- src/app/login-guard.service.ts
+++ src/app/login-guard.service.ts
@@ -20,11 +20,10 @@ export class LoginGuardService implements CanActivate, CanActivateChild{
     state: RouterStateSnapshot
   ): boolean|UrlTree  {
     let isLogin: boolean;
-    // 判断用户是否登入
+    // 判断用户是否登入，没有这两个字符都视作没有登录
     const user = sessionStorage.getItem("token");
-    //判断有没有权限
-    const permisssions = sessionStorage.getItem("permissions");
-    //没有权限或者没有登录都要跳到登录页
+    const permisssions = sessionStorage.getItem("permissions"); 
+    //没有登录强制导航到登录页
     if (!user||!permisssions) {
       return this.router.parseUrl("/login");    
     } else {
@@ -32,6 +31,7 @@ export class LoginGuardService implements CanActivate, CanActivateChild{
     }
     return isLogin;
   }
+  //
   canActivateChild(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot){  
@@ -39,22 +39,21 @@ export class LoginGuardService implements CanActivate, CanActivateChild{
     let isLogin: boolean;
     // 判断用户是否登入
     const user = sessionStorage.getItem("token");
-     //判断有没有权限
-     const permisssions = sessionStorage.getItem("permissions");
+    const permisssions = sessionStorage.getItem("permissions");
     //没有登陆并且没有权限但有vim，要带上参数跳到登录页
-      if ((!user||!permisssions) && window.location.search.indexOf("vim") != -1 ) {
-      let p = window.location.pathname.slice(1)
-      let name = p.replace(/\//g, '&')
-      return  this.router.parseUrl("/login" + "?" + name + "&" + "type=vim");//要把pathname中的斜杠替换成&
-      }
-    //没有登陆，直接跳到登录页
-      else if (!user ||!permisssions) {   
-        return this.router.parseUrl("/login");     
-      }
-      else {
-        isLogin = true;
-      }
-     return isLogin;
+    if ((!user||!permisssions) && window.location.search.indexOf("vim") != -1 ) {
+    let p = window.location.pathname.slice(1)
+    let name = p.replace(/\//g, '&')
+    return  this.router.parseUrl("/login" + "?" + name + "&" + "type=vim");//要把pathname中的斜杠替换成&
+    }
+  //没有登陆，直接跳到登录页
+    else if (!user ||!permisssions) {   
+      return this.router.parseUrl("/login");     
+    }
+    else {
+      isLogin = true;
+    }
+    return isLogin;
   }
   
 }
